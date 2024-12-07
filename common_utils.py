816e9b5091d5bab3d020d41f17385ccae4558bf3
RandallCher <100952571+RandallCher@users.noreply.github.com>
2024-12-07 21:32:12 +0800

    first commit

--- common_utils.py
+++ common_utils.py
@@ -1,7 +1,7 @@
 #### THIS FILE CONTAINS COMMON FUNCTIONS AND CLASSES
 
 
-
+from torch import nn 
 from sklearn import preprocessing
 
 samples_normalised_2m = None
@@ -14,3 +14,31 @@ def preprocess_dataset(df_train, df_test):
     df_test_scaled = standard_scaler.transform(df_test)
 
     return df_train_scaled, df_test_scaled
+
+
+class MLPBuilder(nn.Module):
+
+    def __init__(self, no_features, layers, no_labels = 64):
+        super().__init__()
+        layer_list = []
+        
+        # Input layer
+        layer_list.append(nn.Linear(no_features, layers[0]))
+        layer_list.append(nn.ReLU())
+        layer_list.append(nn.Dropout(p=0.2))
+
+        # Hidden layers
+        for i in range(len(layers) - 1):
+            layer_list.append(nn.Linear(layers[i], layers[i+1]))
+            layer_list.append(nn.ReLU())
+            layer_list.append(nn.Dropout(p=0.2))
+
+        # Output layer, 2 outputs
+        layer_list.append(nn.Linear(layers[-1], 2))
+ 
+        # Define the MLP stack as a sequential model
+        self.mlp_stack = nn.Sequential(*layer_list)
+
+    def forward(self, x):
+        logits = self.mlp_stack(x)
+        return logits
---
+++ journal.md
@@ -0,0 +1,3 @@
+## Development Journal
+- **Best Model**: Best performing model is doscovered as layers=[256,128,32]
+- 

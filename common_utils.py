d2f19fbce716455d765a921f8af5cfed8eeb7add
RandallCher <100952571+RandallCher@users.noreply.github.com>
2025-03-03 20:49:16 +0800

    All Models trained

--- common_utils.py
+++ common_utils.py
@@ -15,46 +15,46 @@ def preprocess_dataset(df_train, df_test):
 
     return df_train_scaled, df_test_scaled
 
-class MLPBuilder(nn.Module):
+# class MLPBuilder(nn.Module):
 
-    def __init__(self, no_features, layers, no_labels = 64):
-        super().__init__()
-        layer_list = []
+#     def __init__(self, no_features, layers, no_labels = 64):
+#         super().__init__()
+#         layer_list = []
         
-        # Input layer
-        layer_list.append(nn.Linear(no_features, layers[0]))
-        layer_list.append(nn.ReLU())
-        layer_list.append(nn.Dropout(p=0.2))
+#         # Input layer
+#         layer_list.append(nn.Linear(no_features, layers[0]))
+#         layer_list.append(nn.ReLU())
+#         layer_list.append(nn.Dropout(p=0.2))
 
-        # Hidden layers
-        for i in range(len(layers) - 1):
-            layer_list.append(nn.Linear(layers[i], layers[i+1]))
-            layer_list.append(nn.ReLU())
-            layer_list.append(nn.Dropout(p=0.2))
+#         # Hidden layers
+#         for i in range(len(layers) - 1):
+#             layer_list.append(nn.Linear(layers[i], layers[i+1]))
+#             layer_list.append(nn.ReLU())
+#             layer_list.append(nn.Dropout(p=0.2))
 
-        # Define the MLP stack as a sequential model
-        self.mlp_stack = nn.Sequential(*layer_list)
+#         # Define the MLP stack as a sequential model
+#         self.mlp_stack = nn.Sequential(*layer_list)
 
-        # Output layer, 1 outputs
-        self.output_x = nn.Linear(layers[-1], 8) 
-        self.output_y = nn.Linear(layers[-1], 8) 
+#         # Output layer, 1 outputs
+#         self.output_x = nn.Linear(layers[-1], 8) 
+#         self.output_y = nn.Linear(layers[-1], 8) 
 
 
-        self._initialize_weights() 
+#         self._initialize_weights() 
         
-    def forward(self, x):
-        features = self.mlp_stack(x)
-        logits_x = self.output_x(features)
-        logits_y = self.output_y(features)
-        return logits_x, logits_y
+#     def forward(self, x):
+#         features = self.mlp_stack(x)
+#         logits_x = self.output_x(features)
+#         logits_y = self.output_y(features)
+#         return logits_x, logits_y
     
-    def _initialize_weights(self):
-        for layer in self.mlp_stack:
-            if isinstance(layer, nn.Linear):
-                # Use Kaiming initialization for ReLU activations
-                nn.init.kaiming_uniform_(layer.weight, nonlinearity='relu')
-                if layer.bias is not None:
-                    nn.init.zeros_(layer.bias)
+#     def _initialize_weights(self):
+#         for layer in self.mlp_stack:
+#             if isinstance(layer, nn.Linear):
+#                 # Use Kaiming initialization for ReLU activations
+#                 nn.init.kaiming_uniform_(layer.weight, nonlinearity='relu')
+#                 if layer.bias is not None:
+#                     nn.init.zeros_(layer.bias)
 
     
 def train_loop(dataloader, model, loss_fn_x, loss_fn_y, optimizer):
